94fb6c1210a0221ffa180b78dc5f1d5ed3f31efe
Satoru GITHUB <satorudiaz@gmail.com>
2019-12-29 20:06:16 -0400

    Solved UVa - 1124

---
+++ UVa/1124.cpp
@@ -0,0 +1,14 @@
+// UVa Online Judge - 1124
+// Author: Satoru Diaz Nakada (Satoruu)
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	char equation[50];
+
+	while (scanf("%[^\n]%*c", equation) != EOF) {
+		printf("%s\n", equation);
+	}
+
+	return 0;
+}
